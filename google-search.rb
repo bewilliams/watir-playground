49bc7611b720277d9901df29e04061d1b348c51f
barrywilliams <bewilliams@williamsgarcia.com>
2014-12-01 10:58:46 -0500

    Updated google search - finding text field properly, and clicking search button properly

--- google-search.rb
+++ google-search.rb
@@ -2,4 +2,5 @@ require 'watir-webdriver'
 
 browser = Watir::Browser.new :chrome
 browser.goto 'http://google.com/'
-browser.text_field(:id => 'gbqfq').set "peanuts\n"
+browser.text_field(:name => 'q').set "peanuts"
+browser.button.click
